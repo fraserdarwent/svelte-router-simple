237b4a0f5510ced23912183c28c8058a62397ada
Fraser Darwent <fraser@darwen.tf>
2020-05-23 18:38:20 +0100

    Update readme

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # svelte-router-simple
+
+## Installation
+
+```bash
+yarn install svelte-router-simple
+```
+
+## Usage
+
+```javascript
+  import Router from "svelte-router-simple";
+
+  const routes = [
+    {
+      exact: "/", // Specify an exact path match
+      component: HomeComponent
+    },
+    {
+      prefix: "/", // Specify a prefix path match, and attempt to match child routes
+      component: NotFound, // Used if no child routes match
+      routes: [
+        { exact: "/videos", component: VideosComponents },
+        { exact: "/pictures", component: PicturesComponent },
+      ],
+    },
+  ];
+</script>
+
+<div id="app">
+  <Router {routes} />
+</div>
+```
--- src/components/Router/Router.svelte
+++ src/components/Router/Router.svelte
@@ -1,46 +1,64 @@
-<script>
-  const log = require("node-log-simple");
+<script context="module">
+  export function go(pathname) {
+    window.location.replace(pathname);
+  }
+</script>
 
+<script>
+  import { onMount } from "svelte";
   export let routes;
 
-  const pathname = window.location.pathname;
+  const log = require("node-log-simple");
 
-  function matchRoute(routes) {
-    log.debug(`[Router] ${JSON.stringify(routes)}`);
-    const match = routes.find(route => {
-      // If prefix matches
-      if (route.exact) {
-        return pathname == route.exact;
-      } else if (route.prefix) {
-        return pathname.startsWith(route.prefix);
-      }
-    });
-    // Return matching component or false
-    return match && (matchRoute(match.routes) || match.component);
-  }
+  // location.subscribe((value) => {
+  //   setUrl(value.route);
+  //   setView(value.route);
+  // });
 
-  const component = matchRoute(routes);
+  let component;
 
-  if (!component) {
-    log.error("[Router] No matching route found");
+  onMount(async () => {
+    if (!validateRoutes()) {
+      component = route(window.location.pathname, routes);
+    } else {
+      log.error("[svelte-router-simple] No matching route found");
+    }
+  });
+
+  function route(pathname, routes = []) {
+    log.debug(`[svelte-router-simple] Routing "${pathname}"`);
+    function match(pathname, routes = []) {
+      const matched = routes.find(route => {
+        // If prefix matches
+        if (route.exact) {
+          return pathname == route.exact;
+        } else if (route.prefix) {
+          return pathname.startsWith(route.prefix);
+        }
+      });
+      // Return matching component or false
+      return matched && (match(pathname, matched.routes) || matched.component);
+    }
+    return match(pathname, routes);
   }
 
   function validateRoutes() {
-    routes.flat(Infinity).forEach(route => {
+    routes.flat(Infinity).every(route => {
       if (!route.component) {
         log.error(
-          `[Router] Route "${route.prefix}" is missing required key "component"`
+          `[svelte-router-simple] Route "${route.prefix}" is missing required key "component"`
         );
+        return false;
       }
-      if (route.exact && 0 < route.routes.length) {
+      if (route.exact && route.routes && 0 < route.routes.length) {
         log.error(
-          `[Router] Route "${route.exact}" of type "exact" should not have routes as they will be ignored`
+          `[svelte-router-simple] Route "${route.exact}" of type "exact" should not have routes as they will be ignored`
         );
+        return false;
       }
+      return true;
     });
   }
-
-  validateRoutes();
 </script>
 
 <div>
