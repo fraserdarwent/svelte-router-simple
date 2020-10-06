bdc28a5f9aaff480f232fc5c879d113d56bebf83
Fraser Darwent <fraser@darwen.tf>
2020-10-06 23:30:41 +0100

    Stash

---
+++ src/components/Router.svelte
@@ -0,0 +1,95 @@
+<script context="module">
+  import "array-flat-polyfill";
+
+  export const go = function(pathname) {
+    console.error(
+      `[@fraserdarwent/svelte-router] Function "go" is deprecated and will be removed in future version, please use "route" instead`
+    );
+    route(pathname);
+  };
+
+  export const route = function(pathname) {
+    window.history.pushState({}, "", pathname);
+    window.dispatchEvent(new Event("pushState"));
+  };
+
+  const newPathname = function(route, pathname) {
+    return pathname.substring(pathname.indexOf("/", 1), pathname.length);
+  };
+
+  const matchRoute = function(route, pathname) {
+    console.log(`Matching route ${route.path} to ${pathname}`);
+
+    if (route.path === "/*") {
+      return true;
+    }
+
+    return pathname.startsWith(route.path);
+  };
+
+  export const matchRoutes = function(routes = [], pathname) {
+    console.log(`Matching ${JSON.stringify(routes)} to ${pathname}`);
+
+    if (routes.length < 1) {
+      return null;
+    }
+
+    const matched = routes.find(route => matchRoute(route, pathname));
+
+    if (matched) {
+      console.log(`Matched ${JSON.stringify(matched)}`);
+      pathname = newPathname(matched, pathname);
+      return matchRoutes(matched.routes, pathname) || matched.component;
+    }
+
+    return null;
+  };
+
+  const matchLocation = function(routes) {
+    return matchRoutes(routes, window.location.pathname);
+  };
+
+  export const validateRoutes = function(routes) {
+    const valid = routes.flat().every(route => {
+      // If route has no component
+      if (!route.component) {
+        console.error(
+          `[@fraserdarwent/svelte-router] All routes must have required key "component"`
+        );
+        return false;
+      }
+      // If route has no exact or prefix
+      if (!route.path) {
+        console.error(
+          `[@fraserdarwent/svelte-router] All routes must have required key "path"`
+        );
+        return false;
+      }
+      return true;
+    });
+    return valid;
+  };
+</script>
+
+<script>
+  import { onMount } from "svelte";
+  export let routes;
+
+  let component;
+
+  onMount(async function() {
+    const validRoutes = validateRoutes(routes);
+    if (validRoutes) {
+      component = matchLocation(routes);
+      window.addEventListener("pushState", function() {
+        component = matchLocation(routes);
+      });
+
+      window.onpopstate = function(event) {
+        component = matchLocation(routes);
+      };
+    }
+  });
+</script>
+
+<svelte:component this={component} />
--- src/tests.js
+++ src/tests.js
@@ -1,14 +1,131 @@
-const log = require("@fraserdarwent/javascript-logger");
-log.setLevel(log.levels.DEBUG);
-const tests = [];
-tests.push(require("./components/Router/tests.js"));
-const errors = tests.flatMap((test) => {
-  return test.run();
+import "svelte/register.js";
+import log from "@fraserdarwent/javascript-logger";
+import assert from "assert";
+import { route, matchRoutes, validateRoutes } from "./components/Router.svelte";
+
+let tests = [];
+
+global.Event = function Event(e) {
+  this.e = e;
+};
+
+global.window = {
+  history: { pushState: () => {} },
+  dispatchEvent: () => {},
+};
+
+tests.push(function testDepthZero() {
+  const routes = [
+    {
+      path: "/",
+      component: "true",
+    },
+  ];
+  try {
+    assert.ok(validateRoutes(routes));
+    assert.equal(matchRoutes(routes, "/"), "true");
+  } catch (error) {
+    return error;
+  }
 });
+
+tests.push(function testDepthOne() {
+  const routes = [
+    {
+      path: "/false",
+      component: "false",
+    },
+    {
+      path: "/true",
+      component: "true",
+    },
+  ];
+  try {
+    assert.ok(validateRoutes(routes));
+    assert.equal(matchRoutes(routes, "/true"), "true");
+  } catch (error) {
+    return error;
+  }
+});
+
+tests.push(function testDepthTwo() {
+  const routes = [
+    {
+      path: "/true",
+      component: "false",
+      routes: [
+        { path: "/true", component: "true" },
+        { path: "/false", component: "false" },
+      ],
+    },
+  ];
+  try {
+    assert.ok(validateRoutes(routes));
+    assert.equal(matchRoutes(routes, "/true/true"), "true");
+    assert.equal(matchRoutes(routes, "/true/false"), "false");
+  } catch (error) {
+    return error;
+  }
+});
+
+tests.push(function testDepthThree() {
+  const routes = [
+    {
+      path: "/true",
+      component: "false",
+      routes: [
+        {
+          path: "/*",
+          component: "false",
+          routes: [{ path: "/true", component: "true" }],
+        },
+      ],
+    },
+  ];
+  try {
+    assert.ok(validateRoutes(routes));
+    assert.equal(matchRoutes(routes, "/true/true/true"), "true");
+    assert.equal(matchRoutes(routes, "/true/true/false"), "false");
+    assert.equal(matchRoutes(routes, "/true/false/false"), "false");
+  } catch (error) {
+    return error;
+  }
+});
+
+tests.push(function testDepthThreeWildcard() {
+  const routes = [
+    {
+      path: "/true",
+      component: "false",
+      routes: [
+        {
+          path: "/*",
+          component: "true",
+          routes: [{ path: "/true", component: "true" }],
+        },
+      ],
+    },
+  ];
+  try {
+    assert.ok(validateRoutes(routes));
+    assert.equal(matchRoutes(routes, "/true/true/true"), "true");
+  } catch (error) {
+    return error;
+  }
+});
+
+const errors = tests.flatMap((test) => {
+  const error = test();
+  if (error) {
+    return `${test.name} ${error}`;
+  }
+});
+
 const success = !errors.some((error) => {
   if (error) {
     log.error(error);
     return true;
   }
 });
+
 process.exit(success ? 0 : 1);
